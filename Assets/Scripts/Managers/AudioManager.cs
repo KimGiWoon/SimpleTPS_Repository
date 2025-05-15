d2c519b8981c519d3d1fbaaf92b7234e2675b16d
KimGiWoon <rldns20704@naver.com>
2025-05-15 14:53:14 +0900

    [Feat] Add AudioManager, SFXController
    오디오 매니저와 SFX 컨트롤러 코드 추가

--- Assets/Scripts/Managers/AudioManager.cs
+++ Assets/Scripts/Managers/AudioManager.cs
@@ -6,8 +6,9 @@ using UnityEngine;
 public class AudioManager : MonoBehaviour
 {
     private AudioSource _bgmSource;
-
-    //private ObjectPool<PlayerStatus> _pool = new();
+    [SerializeField] List<AudioClip> _bgmList = new();
+    [SerializeField] SFXController _sfxPrefab;
+    ObjectPool _sfxPool;
 
     private void Awake()
     {
@@ -17,10 +18,24 @@ public class AudioManager : MonoBehaviour
     private void Init()
     {
         _bgmSource = GetComponent<AudioSource>();
+        _sfxPool = new ObjectPool(_sfxPrefab, 10);
     }
 
-    public void BgmPlay()
+    public void BgmPlay(int index)
     {
-        _bgmSource.Play();
+        if(0 <= index && index < _bgmList.Count)
+        {
+            _bgmSource.Stop();
+            _bgmSource.clip = _bgmList[index];
+            _bgmSource.Play();
+        }
+    }
+
+    public SFXController GetSFX()
+    {
+        // Pool 에서 Pop하고 변환
+        // SFXController sfx = _sfxPool.PopPool() as SFXController; 도 가능
+        PooledObject poolObj = _sfxPool.PopPool();
+        return poolObj as SFXController;    // SFX타입으로 변환하여 반환
     }
 }
